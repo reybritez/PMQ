b4bab1e096b4e1eb00fd3aaba9d5cd124b6556a0
Steven Rey <stevenreybritez@gmail.com>
2023-03-01 16:31:58 -0300

    cliente

---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,38 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Publicidad
+        </Typography>
+        <Typography color={medium}>Promociona acá ;)</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="http://localhost:3001/assets/info3.jpeg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>CosmeticosAsuncion</Typography>
+        <Typography color={medium}>cosmetics.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id
+        rhoncus nisi. Donec nec mi magna. Morbi varius interdum quam.
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default AdvertWidget;
